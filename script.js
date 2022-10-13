12322abf921ce145cdbc4fa76fc8c2e4bf067492
nickwilmas1 <nwilmas@yahoo.com>
2022-10-13 16:17:11 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+/* Nick Wilmas MODULE 5 Challenge */
+// Get the current date
+
+$(document).ready(function(){
+
+    var date = moment().format('dddd, MMM Do YYYY');
+    $("#currentDay").text(date);
+    $(".saveBtn").on("click", function () {
+        // Get nearby values of the description in JQuery
+        console.log(this)
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // Save text in local storage
+        localStorage.setItem(time, text);
+    })
+    //load 
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour1 .description").val(localStorage.getItem("hour1"));
+    $("#hour2 .description").val(localStorage.getItem("hour2"));
+    $("#hour3 .description").val(localStorage.getItem("hour3"));
+    $("#hour4 .description").val(localStorage.getItem("hour4"));
+    $("#hour5 .description").val(localStorage.getItem("hour5"));
+
+})
